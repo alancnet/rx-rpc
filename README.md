1f10dbd2f24517f01c66528be8b405abbb230efd
Alan Colon <acolon@lifetimefitness.com>
2016-04-05 12:02:36 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# rx-rpc
+
+## Setting up Dev
+
+```
+npm install -g mocha-cli
+npm install
+npm test
+```
---
+++ test/index.js
@@ -0,0 +1,77 @@
+'use strict';
+const rx = require('rx');
+const Observable = rx.Observable;
+const chai = require('chai');
+chai.config.includeStack = true;
+global.expect = chai.expect;
+
+
+const RxRpc = require('..').RxRpc;
+describe('something', () => {
+    it('should return non-observable data types as Observable.just', () => {
+        const logic = {
+            echo(v) {
+                return v;
+            }
+        };
+        const left = new RxRpc({provider: logic});
+        const right = new RxRpc({provider: logic});
+        left.couple(right);
+        var ret = null;
+        left.call('echo', 1).forEach(r => ret = r);
+        expect(ret).to.equal(1);
+    });
+
+    it('should pipe returned observables', () => {
+        const logic = {
+            echo(v) {
+                return Observable.just(v).repeat();
+            }
+        };
+        const left = new RxRpc({provider: logic});
+        const right = new RxRpc({provider: logic});
+        left.couple(right);
+        var ret = [];
+        left.call('echo', 1).take(5).forEach(r => ret.push(r));
+        expect(ret.join(',')).to.equal('1,1,1,1,1');
+    });
+
+    it('should pipe passed observables', () => {
+        const left = new RxRpc({provider: {
+
+        }});
+        const right = new RxRpc({provider: {
+            splitStrings(obsOfStrings) {
+                return obsOfStrings.map(str => Observable.from(str.split('')));
+            }
+        }});
+
+        left.couple(right);
+        let count = 0;
+        left.call('splitStrings', Observable.from(['Hello', 'World'])).subscribe(result => {
+            result.forEach(c => count++);
+        })
+        expect(count).to.equal(10);
+
+    })
+    it('should remove completed observables', () => {
+        const left = new RxRpc({provider: {
+
+        }});
+        const right = new RxRpc({provider: {
+            splitStrings(obsOfStrings) {
+                return obsOfStrings.map(str => Observable.from(str.split('')));
+            }
+        }});
+
+        left.couple(right);
+        let count = 0;
+        left.call('splitStrings', Observable.from(['Hello', 'World'])).subscribe(result => {
+            result.forEach(c => count++);
+        })
+        expect(Object.keys(right.observables).length).to.equal(0);
+        expect(Object.keys(left.streams).length).to.equal(0);
+        expect(Object.keys(right.subscriptions).length).to.equal(0);
+
+    })
+})
